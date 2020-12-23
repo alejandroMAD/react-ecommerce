68d2a62518227fd0c18f02505a0b78ccf129e601
Alejandro <alejandro86mg@gmail.com>
2020-12-23 16:48:18 +0100

    built base structure for account components

---
+++ src/components/account/accountInformation.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class AccountInformation extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Account</h1>
+        <h3>Information</h3>
+      </div>
+    );
+  }
+}
+
+export default AccountInformation;
---
+++ src/components/account/purchaseHistory.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class PurchaseHistory extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Purchase</h1>
+        <h3>History</h3>
+      </div>
+    );
+  }
+}
+
+export default PurchaseHistory;
