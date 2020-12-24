4ae191ce486a416b0d0865bf0ef778be8a059fd9
Alejandro <alejandro86mg@gmail.com>
2020-12-24 23:45:40 +0100

    setup purchaseHistory child components and correspondent styles

---
+++ src/components/account/purchaseDetail.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class PurchaseDetail extends Component {
+  render() {
+    return <div>Purchase detail goes here...</div>;
+  }
+}
+
+export default PurchaseDetail;
