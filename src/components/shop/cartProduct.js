1beddb9d2ca7cde3d70c6dab9342377e8a365973
Alejandro <alejandro86mg@gmail.com>
2020-12-30 19:53:13 +0100

    setup cartProduct model and action creator to fetch and set user cart products

--- src/components/shop/cartProduct.js
+++ src/components/shop/cartProduct.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 
 import Quantity from "../quantity";
+import GreenPriceTag from "../greenPriceTag";
 
 class CartProduct extends Component {
   render() {
-    const { className, title, quantity } = this.props;
+    const { className, title, quantity, price } = this.props;
     return (
       <div className={`${className} cart-product`}>
         <img
@@ -14,7 +15,7 @@ class CartProduct extends Component {
         <div className="cart-product__title">{title}</div>
         <Quantity className="cart-product__quantity" quantity={quantity} />
         <a className="cart-product__remove">Remove</a>
-        <GreenPriceTag className="cart-product__price" price={price} />
+        <GreenPriceTag className="cart-product__price" title={price} />
       </div>
     );
   }
